6f3fe499d374442cebd2a2ca55f72991a643de5d
DK\obr <obr@sitecore.net>
2017-09-26 11:43:32 +0300

    Init (original code)

---
+++ src/Sitecore.Support.185484/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.185484")]
+[assembly: AssemblyProduct("Sitecore.Support.185484")]
+[assembly: ComVisible(false)]
